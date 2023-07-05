77510589b6e6afb562bc37c8eb87b01adcd96238
ASTIK <astik.ytt@gmail.com>
2023-07-05 17:05:42 +0200

    Add Ninja Forms

---
+++ wp-content/plugins/ninja-forms/includes/Admin/RestControllerSafeList.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace NinjaForms\Includes\Admin;
+
+/**
+ * Configuration of what can be called and extended by NF_AJAX_REST_Controller
+ */
+class RestControllerSafeList
+{
+
+    const   ALLOWED_METHOD_CALLS = [
+        'NF_AJAX_Controllers_Form' => [
+            'delete'
+        ]
+    ];
+
+    public static function isClassMethodAllowed(string $fqcn, string $method): bool
+    {
+        $return = false;
+
+        if(isset(self::ALLOWED_METHOD_CALLS[$fqcn])
+        && in_array($method,self::ALLOWED_METHOD_CALLS[$fqcn])
+        ){
+            $return = true;
+        } 
+
+        return $return;
+    }
+}
