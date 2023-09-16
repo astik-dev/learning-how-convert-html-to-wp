f66109ecef1544d13d52d799fa6d3aeb2d21cba0
ASTIK <astik.ytt@gmail.com>
2023-09-17 00:34:03 +0200

    Add 'post-blog' post template

--- wp-content/themes/sparrow/page.php
+++ wp-content/themes/sparrow/page.php
@@ -41,39 +41,9 @@
               query_posts( $args );
               while ( have_posts() ) {
                 the_post();
-                ?>
 
-                   <article class="post">
-
-                     <div class="entry-header cf">
-
-                        <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
-
-                        <p class="post-meta">
-
-                           <time class="date" datetime="2014-01-14T11:24"><?php the_time('M j, Y'); ?></time>
-                           /
-                           <span class="categories">
-                              <?php the_category( $separator = " / ", "" ); ?>
-                           </span>
-
-                        </p>
-
-                     </div>
-
-                     <div class="post-thumb">
-                        <a href="<?php the_permalink(); ?>" title=""><?php the_post_thumbnail( "post_thumb" ); ?></a>
-                     </div>
-
-                     <div class="post-content">
-
-                        <?php the_excerpt(); ?>
-
-                     </div>
-
-                  </article> <!-- post end -->
-
-                <?php
+                get_template_part( "post-templates/post", "blog" );
+                   
               } ?>
               
               <?php the_posts_pagination(); ?>
--- wp-content/themes/sparrow/search.php
+++ wp-content/themes/sparrow/search.php
@@ -26,39 +26,13 @@
 
 
             <!-- Вывод постов -->
-            <?php if (have_posts()) { while (have_posts()) { the_post(); ?>
+            <?php if (have_posts()) { while (have_posts()) {
+
+               the_post();
                
-               <article class="post">
+               get_template_part( "post-templates/post", "blog" );
 
-                  <div class="entry-header cf">
-
-                     <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
-
-                     <p class="post-meta">
-
-                        <time class="date" datetime="2014-01-14T11:24"><?php the_time('M j, Y'); ?></time>
-                        /
-                        <span class="categories">
-                           <?php the_category( $separator = " / ", "" ); ?>
-                        </span>
-
-                     </p>
-
-                  </div>
-
-                  <div class="post-thumb">
-                     <a href="<?php the_permalink(); ?>" title=""><?php the_post_thumbnail( "post_thumb" ); ?></a>
-                  </div>
-
-                  <div class="post-content">
-
-                     <?php the_excerpt(); ?>
-
-                  </div>
-
-               </article> <!-- post end -->
-
-            <?php } /*while*/ ?>
+            } /*while*/ ?>
 
                <?php the_posts_pagination(); ?>
 
